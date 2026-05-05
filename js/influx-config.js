77a1be7c1ac707439b1549a5565dfe1bac3a7f05
Dani-Ald <230110150@itsoeh.edu.mx>
2026-05-05 13:00:45 -0600

    Historico corregido

--- js/influx-config.js
+++ js/influx-config.js
@@ -1,4 +1,4 @@
-import { InfluxDB } from 'https://unpkg.com/@influxdata/influxdb-client-browser/dist/index.browser.js';
+import { InfluxDB } from 'https://cdn.jsdelivr.net/npm/@influxdata/influxdb-client-browser@2.10.1/dist/index.browser.js';
 
 const token = '6SUkG-KSMvX4RIRkPUQBuaVbM9txhMaehtKUFqoA5h4bR-I2AA-ouDQLUS0013OcPIjANf3z1gZPb9ctM9waaQ==';
 const org = 'vet';
